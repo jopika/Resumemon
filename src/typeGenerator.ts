b5195e095caf1d6a791fce5b83686a4dbb88a0e7
Stevens Qiu <stevensqiu@gmail.com>
2019-01-27 02:54:55 -0800

    uncommented code

--- src/typeGenerator.ts
+++ src/typeGenerator.ts
@@ -1,45 +1,45 @@
 import {Buzzwords} from "./Buzzwords";
 
 export function getType(): PokeType {
-        // let random: number = Math.floor(Math.random() * 18 + 1);
-        // switch (random) {
-        //     case 1:
-        //         return PokeType.steel;
-        //     case 2:
-        //         return PokeType.ghost;
-        //     case 3:
-        //         return PokeType.fire;
-        //     case 4:
-        //         return PokeType.poison;
-        //     case 5:
-        //         return PokeType.fairy;
-        //     case 6:
-        //         return PokeType.water;
-        //     case 7:
-        //         return PokeType.dragon;
-        //     case 8:
-        //         return PokeType.electric;
-        //     case 9:
-        //         return PokeType.flying;
-        //     case 10:
-        //         return PokeType.ground;
-        //     case 11:
-        //         return PokeType.dark;
-        //     case 12:
-        //         return PokeType.bug;
-        //     case 13:
-        //         return PokeType.fighting;
-        //     case 14:
-        //         return PokeType.normal;
-        //     case 15:
-        //         return PokeType.rock;
-        //     case 16:
-        //         return PokeType.grass;
-        //     case 17:
-        //         return PokeType.psychic;
-        //     case 18:
-        //         return PokeType.ice;
-        // }
+        let random: number = Math.floor(Math.random() * 18 + 1);
+        switch (random) {
+            case 1:
+                return PokeType.steel;
+            case 2:
+                return PokeType.ghost;
+            case 3:
+                return PokeType.fire;
+            case 4:
+                return PokeType.poison;
+            case 5:
+                return PokeType.fairy;
+            case 6:
+                return PokeType.water;
+            case 7:
+                return PokeType.dragon;
+            case 8:
+                return PokeType.electric;
+            case 9:
+                return PokeType.flying;
+            case 10:
+                return PokeType.ground;
+            case 11:
+                return PokeType.dark;
+            case 12:
+                return PokeType.bug;
+            case 13:
+                return PokeType.fighting;
+            case 14:
+                return PokeType.normal;
+            case 15:
+                return PokeType.rock;
+            case 16:
+                return PokeType.grass;
+            case 17:
+                return PokeType.psychic;
+            case 18:
+                return PokeType.ice;
+        }
         
 }
 
