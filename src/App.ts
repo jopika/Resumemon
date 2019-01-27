4bfae2751af0add706e7d8a4d2663267525c895f
Stevens Qiu <stevensqiu@gmail.com>
2019-01-27 04:51:34 -0800

    minor bug

--- src/App.ts
+++ src/App.ts
@@ -39,7 +39,7 @@ app.post('/', upload.array('file-to-upload', 2), (req, res) => {
         pokemons.push(parse(pdfData, globalBuzzwords).then(function (buzzwords: any) {
             let powerLevel: number = generatePowerLevel(buzzwords);
             let moveSet: Set<Move> = generateMoveSet(buzzwords);
-            let type: PokeType = getType(buz);
+            let type: string = getType(buzzwords);
             let name: string = getPokemonByType(type as string);
             let imgUrl: string = getPokemonImageString(name);
             let pokemon: Pokemon = new Pokemon(name, powerLevel, type, moveSet, imgUrl);
