3b3711b29be2d005e6e428e04f6732636f4039d2
Jinny Byun <jinnybyun@gmail.com>
2019-01-26 20:29:39 -0800

    Finished resume parse + generating Buzzword object

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import {parse} from "./parse";
+const fs = require('fs');
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+export function run() {
+    let pdfData = fs.readFileSync('../jinnybyun_resume.pdf');
+    let buzzwordsData = fs.readFileSync('buzzwords.txt').toString().trim().toLowerCase();
+    let globalBuzzwords = buzzwordsData.split('\n');
+
+    parse(pdfData, globalBuzzwords).then(function(buzzwords: any){
+        console.log(buzzwords);
+    });
+}
