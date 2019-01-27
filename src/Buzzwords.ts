4cfb371c2c07c84bce7a0aa44ba5b0d59560f712
Stevens Qiu <stevensqiu@gmail.com>
2019-01-26 17:49:16 -0800

    added totalBuzz and implemented naive getPowerLevel()

--- src/Buzzwords.ts
+++ src/Buzzwords.ts
@@ -1,9 +1,11 @@
 export class Buzzwords {
     buzzwords: {[key: string]: number};
     nonBuzz: number;
+    totalBuzz: number;
 
     constructor() {
         this.buzzwords = {};
         this.nonBuzz = 0;
+        this.totalBuzz = 0;
     }
 }
--- src/powerLevel.ts
+++ src/powerLevel.ts
@@ -1,3 +1,9 @@
-export function generatePowerLevel(buzzwords: object) {
-    
+import { Buzzwords } from "./Buzzwords";
+
+export function generatePowerLevel(buzzwords: Buzzwords) {
+    let powerLevel: number;
+
+    for (let word in buzzwords.buzzwords) {
+        powerLevel += buzzwords.buzzwords[word];
+    }
 }
