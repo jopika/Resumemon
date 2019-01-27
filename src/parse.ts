c6881fea92ed21b7b1c90672f3ab58545ebc4147
Jinny Byun <jinnybyun@gmail.com>
2019-01-26 18:44:33 -0800

    Parse resume into text and count occurrences

--- src/parse.ts
+++ src/parse.ts
@@ -1,7 +1,27 @@
-/**
- * Created by jinny on 1/26/19.
- */
+import {Buzzwords} from "./Buzzwords";
+const pdf = require('pdf-parse');
+const fs = require('fs');
 
 export function parse() {
+    let dataBuffer = fs.readFileSync('/home/jinny/jinnybyun_resume.pdf');
+
+    pdf(dataBuffer).then(function(data: any) {
+        let resume: string = data.text;
+        resume = resume.toLowerCase();
+
+        let buzzwords: Buzzwords = new Buzzwords();
+        let totalBuzzwords: number = 0;
+        let globalBuzzwords = ['software engineering', 'president'];
+        for (let word of globalBuzzwords) {
+            let regexp = new RegExp(word, 'g');
+            let count: number = (resume.match(regexp) || []).length;
+            buzzwords.buzzwords[word] = count;
+            totalBuzzwords += count;
+        }
+        buzzwords.totalBuzz = totalBuzzwords;
+        console.log(buzzwords);
+    });
+
 
 }
+parse();
