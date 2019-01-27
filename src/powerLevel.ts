9b549d34a8fa58da8c20a4731aacd7c608e56cf4
Stevens Qiu <stevensqiu@gmail.com>
2019-01-26 18:45:22 -0800

    implemented power level

--- src/powerLevel.ts
+++ src/powerLevel.ts
@@ -1,9 +1,49 @@
 import { Buzzwords } from "./Buzzwords";
 
-export function generatePowerLevel(buzzwords: Buzzwords) {
-    let powerLevel: number;
+const optimalBuzzRepeats = 3;
+const allowedBuzzCoefficient = 0.3;
+const repeatWeight = 0.8;
+const buzzCoefficientWeight = 0.2;
+const maxRandomPowerDecrementor = 10;
 
-    for (let word in buzzwords.buzzwords) {
-        powerLevel += buzzwords.buzzwords[word];
+export function generatePowerLevel(buzzwords: Buzzwords): number {
+    let repeatScore: number = calculateRepeatScore(buzzwords.buzzwords);
+
+    let buzzCoefficientScore: number = 
+    calculateBuzzCoefficientScore(buzzwords.nonBuzz, buzzwords.totalBuzz);
+    
+    let maxPowerLevel = repeatScore * repeatWeight + 
+    buzzCoefficientScore * buzzCoefficientWeight;
+
+    let powerLevel = randomizePower(maxPowerLevel);
+    return powerLevel;
+}
+
+function calculateRepeatScore(buzzwords: {[key: string]: number}) {
+    let totalRepeatScore = 0;
+    for (let word in buzzwords) {
+        let wordScore = buzzwords[word];
+        if (buzzwords[word] > optimalBuzzRepeats) {
+            wordScore = optimalBuzzRepeats - buzzwords[word];
+        }
+        totalRepeatScore += wordScore;
     }
+    totalRepeatScore = Math.min(totalRepeatScore, 100);
+    totalRepeatScore = Math.max(totalRepeatScore, 1);
+
+    return totalRepeatScore;
+}
+
+function calculateBuzzCoefficientScore(nonBuzz: number, totalBuzz: number): number {
+    let buzzCoefficient: number = totalBuzz/(totalBuzz + nonBuzz);
+    let buzzCoefficientDiff = Math.abs(buzzCoefficient - allowedBuzzCoefficient);
+    let buzzCoefficientScore = (1 - buzzCoefficientDiff) * 100;
+    return buzzCoefficientScore;
+}
+
+function randomizePower(maxPowerLevel: number) {
+    let randomDecrementor = 
+    Math.floor((Math.random() * maxRandomPowerDecrementor) + 1);
+    let powerLevel = Math.max(1, maxPowerLevel - randomDecrementor);
+    return powerLevel;
 }
